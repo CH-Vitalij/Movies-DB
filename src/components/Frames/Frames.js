dbe24fec4662aadd59bc4320ab9553059efc6eb7
CH-Vitalij <v.charkin2017@gmail.com>
2024-07-03 16:31:51 +0500

    save

--- src/components/Frames/Frames.js
+++ src/components/Frames/Frames.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
-import { List, Typography } from 'antd';
+import { List, Typography, Layout, Image, DatePicker } from 'antd';
+import { format } from 'date-fns';
 
 import MoviesService from '../../services/Movies-service';
 import './Frames.css';
@@ -22,30 +23,44 @@ export default class Frames extends Component {
 
   render() {
     // console.log(this.state);
+    const { Paragraph, Title } = Typography;
+
     return (
       <List
-        className="movie-list"
+        className="movies-list"
         itemLayout="horizontal"
         dataSource={this.state.items}
         renderItem={(item) => (
-          <List.Item className="movie-item">
-            <img
+          <List.Item className="movies-list__frame frame">
+            <Image
               width={183}
               height={281}
               alt={item.title}
               src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}
             />
-            <Typography.Paragraph
-              className="overview"
-              ellipsis={{
-                rows: 6,
-                expandable: true,
-                symbol: '',
-                tooltip: `${item.overview}`,
-              }}
-            >
-              {item.overview}
-            </Typography.Paragraph>
+            <Layout style={{ width: '228px', backgroundColor: '#ffffff' }}>
+              <Title level={5} className="frame__title">
+                {item.title}
+              </Title>
+              <div style={{ paddingBottom: '7px' }}>{format(new Date(item.release_date), 'MMMM d, yyyy')}</div>
+              <List
+                className="genres-list"
+                itemLayout="horizontal"
+                dataSource={['Drama']}
+                renderItem={(item) => <List.Item className="genres-list__item">{item}</List.Item>}
+              />
+              <Paragraph
+                className="frame__overview"
+                ellipsis={{
+                  rows: 6,
+                  expandable: true,
+                  symbol: '',
+                  tooltip: `${item.overview}`,
+                }}
+              >
+                {item.overview}
+              </Paragraph>
+            </Layout>
           </List.Item>
         )}
       />
